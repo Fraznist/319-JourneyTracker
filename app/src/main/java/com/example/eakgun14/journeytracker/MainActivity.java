322a67f75870e60528dd98a91beb73bf61b787d1
EAKGUN14 <akgunemr@gmail.com>
2018-04-09 23:07:37 +0300

    Added Most Activities

--- app/src/main/java/com/example/eakgun14/journeytracker/MainActivity.java
+++ app/src/main/java/com/example/eakgun14/journeytracker/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.eakgun14.journeytracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
+
+        Button manageJourneysButton = (Button) findViewById(R.id.manage_journey);
+        manageJourneysButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, JournalsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button startJourneyButton = (Button) findViewById(R.id.start_journey);
+        manageJourneysButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StartJourneyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button viewJourneysButton = (Button) findViewById(R.id.manage_journey);
+        viewJourneysButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewJourneysActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/eakgun14/journeytracker/ViewJourneysActivity.java
@@ -0,0 +1,13 @@
+package com.example.eakgun14.journeytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewJourneysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_journeys);
+    }
+}
